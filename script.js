66a6848af166b3bd37fcdaefc7764f72217c283b
Abha1079 <113463346+Abha1079@users.noreply.github.com>
2025-10-15 16:17:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 // Complete the js code
 function Car(make, model) {
-	this.make = make,
+        
+	    this.make = make,
 		this.model = model
 }
 Car.prototype.getMakeModel = function () {
 	return `${this.make}${this.model}`
 }
-function SportsCar extends Car(make, model, topSpeed) {
-	super(this.make,this.model)
+
+SportsCar.prototype = Car.prototype
+
+function SportsCar (make, model, topSpeed) {
+
+	 
 	this.topSpeed = topSpeed
 	
 	
 }
 
- SportsCar.prototype.getMakeModel = function (){
-	 return this.topSpeed
- }
+SportsCar.prototype.topSpeed = function(){
+	return `${this.topSpeed }`
+}
+ 
 
 // Do not change the code below
 window.Car = Car;
